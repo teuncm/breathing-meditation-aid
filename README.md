180313dc182c664624ec07e90ced826c6d941ae0
teuncm <teun.c.mathijssen@gmail.com>
2024-07-16 20:36:05 +0200

    Add empty Vue template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Breathing Meditation Aid
+
+Sometimes you find yourself overwhelmed. All you need is a breathing break. Enjoy this free mobile-friendly no-install audiovisual meditation aid.
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import Meditation from './components/Meditation.vue';
+import Settings from './components/Settings.vue';
+export default {
+  name: 'App',
+  components:
+  {
+    Meditation,
+    Settings,
+  },
+};
+</script>
+
+<template>
+
+</template>
