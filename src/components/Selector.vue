0824e9a993fa63d09dd72cad199ad8ca925b1b95
teuncm <teun.c.mathijssen@gmail.com>
2024-07-17 11:15:18 +0200

    Add selector

---
+++ src/components/Selector.vue
@@ -0,0 +1,24 @@
+<script setup>
+const model = defineModel()
+
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  min: {
+    type: Number,
+    required: true
+  },
+  max: {
+    type: Number,
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+});
+</script>
+
+<template>
+  <input v-model.number="model" value="1" type="number" min="1" max="5" step="1" />
+</template>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,3 +1,22 @@
-<template>
+<script>
+import Selector from './Selector.vue';
 
+export default {
+  components:
+  {
+    Selector,
+  },
+  data () {
+    return  {
+      test: 4,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="settings-container">
+    <Selector v-model="test"/>
+    <Selector v-model="test"/>
+  </div>
 </template>
